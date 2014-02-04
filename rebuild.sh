c04b399262b030f20d09c80d8634398a9ab32f41
Henri Lakk <henri.lakk@guardtime.com>
2014-02-04 10:09:06 +0200

    Added rebuild template.

---
+++ rebuild.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+PRF=libksi-0.0.1
+
+rm -f ${PRF}*.tar.gz && \
+mkdir -p config m4 && \
+echo Running autoreconf... && \
+autoreconf -i && \
+echo Running configure script... && \
+./configure $* && \
+echo Running make... && \
+make clean && \
+make && \
+make check && \
+make test \
+#make doc && \
+#echo Packaging sources... && \
+#make dist && \
+#echo Packaging binaries... && \
+#rm -rf ./${PRF} && \
+#make install prefix=$(pwd)/${PRF} && \
+#tar -czvf ${PRF}-bin.tar.gz ./${PRF} && \
+#echo Packaging documentation... && \
+#rm -rf ./${PRF} && \
+#mkdir -p ./${PRF}/doc && \
+#cp -r ./doc/html ./doc/latex/refman.pdf ./${PRF}/doc && \
+#tar -czvf ${PRF}-doc.tar.gz ./${PRF}
