da682d0e5e7db42543df9972c8befff5c4ba3a4f
Henri Lakk <henri.lakk@guardtime.com>
2014-02-06 17:37:28 +0200

    Added header file for all tests.

---
+++ test/all_tests.h
@@ -0,0 +1,22 @@
+/*
+ * all_tests.h
+ *
+ *  Created on: 06.02.2014
+ *      Author: henri
+ */
+
+#ifndef ALL_TESTS_H_
+#define ALL_TESTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+CuSuite* KSI_CTX_GetSuite(void);
+CuSuite* KSI_LOG_GetSuite(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ALL_TESTS_H_ */
