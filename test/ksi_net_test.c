afc2eb40a07c711d9a20196ac631b3efb121bd9d
Henri Lakk <henri.lakk@guardtime.com>
2014-06-30 10:25:52 +0300

    Removed status dump.

--- test/ksi_net_test.c
+++ test/ksi_net_test.c
@@ -47,6 +47,7 @@ static void testSigning(CuTest* tc) {
 	KSITest_setFileMockResponse(tc, "test/resource/tlv/ok_aggr_response-1.tlv");
 
 	res = KSI_createSignature(ctx, hsh, &sig);
+	KSI_ERR_statusDump(ctx, stdout);
 	CuAssert(tc, "Unable to sign the hash", res == KSI_OK && sig != NULL);
 	CuAssert(tc, "Unexpected send request", KSI_NET_MOCK_request_len == sizeof(expectedSignRequest) && !memcmp(expectedSignRequest, KSI_NET_MOCK_request, KSI_NET_MOCK_request_len));
 	KSI_free(KSI_NET_MOCK_response);
