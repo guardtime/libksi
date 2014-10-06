7cdd96c239d7943063988551bfd51b9dde6250fd
Henri Lakk <henri.lakk@guardtime.com>
2014-10-06 12:15:27 +0300

    Default log is set to debug and piped to ksi.log

--- src/ksi/base.c
+++ src/ksi/base.c
@@ -141,7 +141,7 @@ int KSI_CTX_new(KSI_CTX **context) {
 	if (res != KSI_OK) goto cleanup;
 
 	/* Create and set the logger. */
-	res = KSI_Logger_new(ctx, NULL, KSI_LOG_FATAL, &logger);
+	res = KSI_Logger_new(ctx, "ksi.log", KSI_LOG_DEBUG, &logger);
 	if (res != KSI_OK) goto cleanup;
 
 	res = KSI_setLogger(ctx, logger);
