2f210493a144486a0bfef2427bd08e3330ca38bc
Henri Lakk <henri.lakk@guardtime.com>
2014-11-24 18:23:42 +0200

    Fixed typo.

--- src/ksi/common.h
+++ src/ksi/common.h
@@ -19,7 +19,7 @@ extern "C" {
 	#if defined(__GNUC__) && ((__GNUC__ >= 4) || ((__GNUC__ == 3) && (__GNUC_MINOR__ >= 1)))
 		#define KSI_FN_DEPRECATED(decl) decl __attribute__((deprecated))
 	#elif defined(_WIN32)
-		#define KSI_FN_DEPRECATED(decl) __declspec(deprecated)) decl
+		#define KSI_FN_DEPRECATED(decl) __declspec(deprecated) decl
 	#else
 		#define KSI_FN_DEPRECATED(decl) decl;
 	#endif
