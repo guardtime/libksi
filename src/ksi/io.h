af2e3be063a5487c4d21272b5407a7e48184d2b8
Henri Lakk <henri.lakk@guardtime.com>
2015-01-13 17:01:20 +0200

    Experimental stage of using tcp connection.

--- src/ksi/io.h
+++ src/ksi/io.h
@@ -22,6 +22,7 @@ extern "C" {
 	 */
 	int KSI_RDR_fromMem(KSI_CTX *ctx, const unsigned char *buffer, const size_t buffer_length, KSI_RDR **rdr);
 	int KSI_RDR_fromSharedMem(KSI_CTX *ctx, unsigned char *buffer, const size_t buffer_length, KSI_RDR **rdr);
+	int KSI_RDR_fromSocket(KSI_CTX *ctx, int socketfd, KSI_RDR **rdr);
 
 	/* TODO!
 	 *
