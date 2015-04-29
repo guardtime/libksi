77754779d309fbd6aad752afe7f003c2ec7911e4
Henri Lakk <henri.lakk@guardtime.com>
2015-04-29 11:47:12 +0100

    Updated licence.

--- src/ksi/types_base.h
+++ src/ksi/types_base.h
@@ -1,21 +1,20 @@
-/**************************************************************************
+/*
+ * Copyright 2013-2015 Guardtime, Inc.
  *
- * GUARDTIME CONFIDENTIAL
+ * This file is part of the Guardtime client SDK.
  *
- * Copyright (C) [2015] Guardtime, Inc
- * All Rights Reserved
- *
- * NOTICE:  All information contained herein is, and remains, the
- * property of Guardtime Inc and its suppliers, if any.
- * The intellectual and technical concepts contained herein are
- * proprietary to Guardtime Inc and its suppliers and may be
- * covered by U.S. and Foreign Patents and patents in process,
- * and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this
- * material is strictly forbidden unless prior written permission
- * is obtained from Guardtime Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES, CONDITIONS, OR OTHER LICENSES OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  * "Guardtime" and "KSI" are trademarks or registered trademarks of
- * Guardtime Inc.
+ * Guardtime, Inc., and no license to trademarks is granted; Guardtime
+ * reserves and retains all trademark rights.
  */
 
 #ifndef TYPES_BASE_H_
