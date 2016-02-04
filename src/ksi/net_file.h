8b86714feef61a17539bd77052349f30aa139eb7
Maksim Tamleht <maksim.tamleht@guardtime.com>
2016-02-04 14:37:04 +0200

    Added raw data pointer initialization. Use ksi context network client request count for calculating request id Updated header file. Converted spaces to tabs.

--- src/ksi/net_file.h
+++ src/ksi/net_file.h
@@ -27,51 +27,53 @@
 extern "C" {
 #endif
 
-    typedef struct KSI_FsClient_st KSI_FsClient;
+	typedef struct KSI_FsClient_st KSI_FsClient;
 
-    /**
-     * Creates a new file system client.
-     * \param[in]	ctx			KSI context.
-     * \param[out]	client		Pointer to the receiving pointer.
-     * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
-     */
-    int KSI_FsClient_new(KSI_CTX *ctx, KSI_NetworkClient **client);
+	/**
+	 * Creates a new file system client.
+	 * \param[in]	ctx			KSI context.
+	 * \param[out]	client		Pointer to the receiving pointer.
+	 * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
+	 */
+	int KSI_FsClient_new(KSI_CTX *ctx, KSI_NetworkClient **client);
 
-    /**
-     * Setter for the publications file URL. The value will be copied and thus
-     * can be freed after successful call.
-     * \param[in]	client		Pointer to the http client.
-     * \param[in]	val			Null-terminated URL.
-     * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
-     */
-    int KSI_FsClient_setPublicationUrl(KSI_NetworkClient *client, const char *path);
+	/**
+	 * Setter for the publications file URL. The value will be copied and thus
+	 * can be freed after successful call.
+	 * \param[in]	client		Pointer to the http client.
+	 * \param[in]	path		Null-terminated file path.
+	 * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
+	 */
+	int KSI_FsClient_setPublicationUrl(KSI_NetworkClient *client, const char *path);
 
 
-    /**
-     * Setter for the file system client extender parameters.
-     * \param[in]	client		Pointer to tcp client.
-     * \param[in]	path		Path to the file.
-     * \param[in]	key			HMAC shared secret.
-     * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
-     */
-    int KSI_FsClient_setExtender(KSI_NetworkClient *client, const char *path, const char *user, const char *pass);
+	/**
+	 * Setter for the file system client extender parameters.
+	 * \param[in]	client		Pointer to tcp client.
+	 * \param[in]	path		Path to the file.
+	 * \param[in]	user		NULL-terminated user name
+	 * \param[in]	pass		NULL-terminated password
+	 * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
+	 */
+	int KSI_FsClient_setExtender(KSI_NetworkClient *client, const char *path, const char *user, const char *pass);
 
-    /**
-     * Setter for the file system aggregator parameters.
-     * \param[in]	client		Pointer to tcp client.
-     * \param[in]	path		Path to the file.
-     * \param[in]	key			HMAC shared secret.
-     * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
-     */
-    int KSI_FsClient_setAggregator(KSI_NetworkClient *client, const char *path, const char *user, const char *pass);
+	/**
+	 * Setter for the file system aggregator parameters.
+	 * \param[in]	client		Pointer to tcp client.
+	 * \param[in]	path		Path to the file.
+	 * \param[in]	user		NULL-terminated user name
+	 * \param[in]	pass		NULL-terminated password
+	 * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
+	 */
+	int KSI_FsClient_setAggregator(KSI_NetworkClient *client, const char *path, const char *user, const char *pass);
 
-    /**
-     * Extract file path from URI
-     * \param[in]   uri         File URI.
-     * \param[out]  path        Path to the file
-     * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
-     */
-    int KSI_FsClient_extractPath(const char *uri, char **path);
+	/**
+	 * Extract file path from URI
+	 * \param[in]   uri         File URI.
+	 * \param[out]  path        Path to the file
+	 * \return status code (#KSI_OK, when operation succeeded, otherwise an error code).
+	 */
+	int KSI_FsClient_extractPath(const char *uri, char **path);
 
 #ifdef __cplusplus
 }
