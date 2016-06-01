1aeb64129da7e989b7d1d54c0b487bf25ffebfb4
MattiasKonkov <mattias.konkov@guardtime.com>
2016-06-01 13:27:55 +0000

    Update WinBuild32.bat: removed accidental memtes target

--- WinBuild32.bat
+++ WinBuild32.bat
@@ -24,6 +24,6 @@ GOTO copyrightend
 CALL "%ProgramW6432%\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x86
 
 nmake clean
-nmake RTL=MTd DLL=lib NET_PROVIDER=WINHTTP CRYPTO_PROVIDER=CRYPTOAPI all memtest 
+nmake RTL=MTd DLL=lib NET_PROVIDER=WINHTTP CRYPTO_PROVIDER=CRYPTOAPI all test 
 
 pause
