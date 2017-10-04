45295668193dd3aecb098287770c588c6bca3cb2
taavi.valjaots <taavi.valjaots@guardtime.com>
2017-10-04 16:09:49 +0300

    Some typos fixed.

--- packaging/deb/libksi/rebuild_changelog.sh
+++ packaging/deb/libksi/rebuild_changelog.sh
@@ -32,27 +32,27 @@ set -e
     echo "  $0 <changelog path> <control file path> <output path> [<distribution guide>]"
     echo ""
     echo "Description:"
-    echo "  This scripts takes a path to regular changelog and debian control files and"
+    echo "  This script takes a path to regular changelog and debian control files and"
     echo "  creates a debian changelog file that is written to the specified output."
     echo ""
     echo ""
     echo "Fields:"
     echo "  changelog path"
-    echo "       - path to changelog file that is used for debian changelog generation."
+    echo "       - Path to changelog file that is used for debian changelog generation."
     echo ""
     echo "  control file path"
     echo "       - Path to debian control file."
     echo ""
     echo "  output path"
-    echo "       - Output path for debian changelog. Will overwrite existsing file!"
+    echo "       - Output path for debian changelog. Will overwrite existing file!"
     echo ""
     echo "  distribution guide"
     echo "        - Array of distribution guides. As debian changelog needs a field for."
     echo "          distribution (e.g. stable, unstable). To change the default values"
-    echo "          a list of relese versions and distribution value pairs must be specified."
+    echo "          a list of release versions and distribution value pairs must be specified."
     echo "          When distribution value changes after a version X, it must only specified at"
-    echo "          once as next releases us the previous distribution value. Note that distribution"
-	echo "          UNREALESED will never be included to the debian changelog."
+    echo "          once as next releases uses the previous distribution value. Note that distribution"
+    echo "          UNREALESED will never be included to the debian changelog."
     echo ""
     echo "          For example '0.1.0:UNRELEASED 1.2.1:unstable 1.2.11:stable' will create a"
     echo "          changelog where first releases were actually never released until 1.2.1 where"
@@ -63,7 +63,7 @@ set -e
     echo "  Rebuild changelog so that every release is marked as default distribution (unstable):"
     echo "    rebuild_changelog.sh changelog packaging/deb/control packaging/deb/changelog"
     echo ""
-    echo "  Rebuild changelog so that some of the first releases are not included (as the releles" 
+    echo "  Rebuild changelog so that some of the first releases are not included (as the release" 
     echo "  were never made under debian):"
     echo "    rebuild_changelog.sh changelog packaging/deb/control packaging/deb/changelog"
     echo "    '0.0.32:UNRELEASED 1.0.0:unstable'"
@@ -99,8 +99,8 @@ while read line; do
   if [[ $line =~ ([0-9]{1,4}[-][0-9]{1,2}[-][0-9]{1,2}).*release.*[(](.*)[)] ]] ; then
     time_string=$(date -R -d "${BASH_REMATCH[1]}")
     version_str="${BASH_REMATCH[2]}"
-    
-	# Check if distribution name has to be changed.
+
+    # Check if distribution name has to be changed.
     for key in "${distribution_quide[@]}"; do
       if [[ $key == "$version_str:"* ]]; then
         dist=$(echo $key| cut -d':' -f 2)
@@ -114,34 +114,34 @@ while read line; do
         echo "  * Changing distribution to: $dist."
       fi
     done;
-	
-	# If distribution is UNRELEASED, just give a warning and do nothing as the records will be ignored anyway.
+
+    # If distribution is UNRELEASED, just give a warning and do nothing as the records will be ignored anyway.
     if $is_unreleased ; then
-		echo "Warning: distribution is 'UNREALESED'. Version $version_str is not included to changelog."
+        echo "Warning: distribution is 'UNREALESED'. Version $version_str is not included to changelog."
     else
-		echo "Appending new release: v$version_str $time_string"
-		
-		# If this is the first release, create empty changelog file.
-		if $is_first ; then
-		  dch --create --package libksi --newversion "$version_str" --urgency low --controlmaint "${array[-1]}"
-		else
-		  dch --controlmaint -v "$version_str" --urgency low "${array[-1]}"
-		fi
-		
-		# Append release record.
-		for ((i=${#array[@]}-2; i>=0; i--)); do
-		  dch -a --nomultimaint --controlmaint "${array[$i]}"
-		done
-			  
-		# Make a release, change time.
-		dch --controlmaint --release $extra_release_commands ""
-		sed -i "0,/>.*/s/>.*/>  $time_string/" debian/changelog
-				
-		is_first=false
-	fi
-	
-	# Clean array.
-	array=()
+        echo "Appending new release: v$version_str $time_string"
+
+        # If this is the first release, create empty changelog file.
+        if $is_first ; then
+          dch --create --package libksi --newversion "$version_str" --urgency low --controlmaint "${array[-1]}"
+        else
+          dch --controlmaint -v "$version_str" --urgency low "${array[-1]}"
+        fi
+
+        # Append release record.
+        for ((i=${#array[@]}-2; i>=0; i--)); do
+          dch -a --nomultimaint --controlmaint "${array[$i]}"
+        done
+
+        # Make a release, change time.
+        dch --controlmaint --release $extra_release_commands ""
+        sed -i "0,/>.*/s/>.*/>  $time_string/" debian/changelog
+
+        is_first=false
+    fi
+
+    # Clean array.
+    array=()
   elif [[ $line == \** ]] ; then
     array+=("${line:2}")
   fi
