13d1acd4b33d86bb3814a21f6ec1c3e10e75fcb6
Maksim Tamleht <maksim.tamleht@guardtime.com>
2018-03-27 15:36:26 +0300

    Increase async safety timeout.

--- test/support_tests.h
+++ test/support_tests.h
@@ -15,7 +15,7 @@ extern "C" {
 #define TEST_SCHEME_TCP "ksi+tcp"
 
 #define KSITEST_ASYNC_SLEEP_TIME_MS 100
-#define KSITEST_ASYNC_NO_RESP_TIMEOUT_MS (KSITEST_ASYNC_SLEEP_TIME_MS * 10)
+#define KSITEST_ASYNC_NO_RESP_TIMEOUT_MS (100 * 10 * 5)
 
 void addSuite(CuSuite *suite, CuSuite* (*fn)(void));
 void printStats(CuSuite *suite, const char *heding);
